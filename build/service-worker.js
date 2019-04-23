5bcda329acbc28093483b3643fa7fe9b71ac49a7
Will Chisholm <william.f.chisholm.iii.20@dartmouth.edu>
2019-04-23 15:12:20 -0400

    done with ws

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.2d36fb55f1b956d2ba6de9b2aa0303d1.js"
 );
 
 workbox.clientsClaim();
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker, Polygon } from 'google-maps-react';
+// AIzaSyByKo2myIRRLGBT51T6Bo9lkl9HDr1UFzA
 
 const mapStyles = {
   width: '100%',
@@ -6,15 +8,83 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        showingInfoWindow: false, //Hides or the shows the infoWindow
+        activeMarker: {}, //Shows the active marker upon click
+        selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+    }; 
+} 
+
+onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+});
+onClose = props => {
+  if (this.state.showingInfoWindow) {
+      this.setState({
+      showingInfoWindow: false,
+      activeMarker: null
+      });
+  }
+};
+
   render() {
+    const triangleCoords = [
+      {lat: 43.7046, lng: -72.2943}, 
+      {lat: 43.7034, lng: -72.2886}, 
+      {lat: 43.7030, lng: -72.2841}, //gym
+      {lat: 43.7091, lng: -72.2839}, 
+      {lat: 43.7074, lng: -72.2911}, //choates
+      {lat: 43.7066, lng: -72.2968}, //ledyard
+      {lat: 43.7046, lng: -72.2943} 
+  ];
     return (
       <div>
         <h1>Hello Google Maps</h1>
+        <Map 
+    google={this.props.google}
+    initialCenter={{
+      lat: 43.7044,
+      lng: -72.2887
+  }}
+  zoom={16}
+> 
+<Marker
+    onClick={this.onMarkerClick}
+    name={'This is a marker we just made yay!'}
+    position={{lat: 43.708962, lng: -72.284357}}
+/>
+<Marker
+    onClick={this.onMarkerClick}
+    name={'This is another marker we just made yay!'}
+    position={{lat: 43.703511, lng: -72.288545}}
+/>
+<InfoWindow marker={this.state.activeMarker}
+visible={this.state.showingInfoWindow}
+onClose={this.onClose}>
+<div>
+    <h2>{this.state.selectedPlace.name}</h2>
+</div>
+</InfoWindow>
+<Polygon
+    paths={triangleCoords}
+    strokeColor="#ff00d9"
+    strokeOpacity={0.6}
+    strokeWeight={5}
+    fillColor="#dd4fc8"
+    fillOpacity={0.3} 
+/>
+</Map>
       </div>
+      
     ) 
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'ENTER_YOUR_API_KEY_HERE'
+  apiKey: 'AIzaSyByKo2myIRRLGBT51T6Bo9lkl9HDr1UFzA'
 })(MapContainer);
